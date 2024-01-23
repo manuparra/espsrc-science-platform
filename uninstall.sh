cf98d46c2c50a492c94e6912ce727ab03d0e8b7d
Manuel Parra <manuparra@gmail.com>
2024-01-23 08:13:09 +0000

    Deleting storage units pv/pvc

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,13 @@
+RED='\033[0;31m'
+NC='\033[0m'
+
 helm uninstall base
 helm uninstall posixmapper
 helm uninstall skaha
-echo "No skaha-system pods installed ..."
+helm uninstall science-platform
+echo "${RED}No skaha-system pods installed ...${NC}"
 kubectl get all -n skaha-system
+echo "${RED}Deleting Storage ...${NC}"
+kubectl delete pv science-platform-volume
+kubectl delete pv science-platform-cavern-volume
 
