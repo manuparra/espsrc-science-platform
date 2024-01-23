f7041a719b58a876279ddda7a1023ab7f05a647c
Manuel Parra <manuparra@gmail.com>
2024-01-23 07:02:57 +0000

    updated install skaha

--- start.sh
+++ start.sh
@@ -11,4 +11,8 @@ echo "Installing science platorm - Base"
 helm upgrade --install base science-platform/base
 echo "Installing science platfor - PosixMapper"
 helm upgrade --install -n skaha-system  --values posix-mapper.yaml posixmapper science-platform/posixmapper
-
+echo "Installing storage"
+kubectl apply -f storage-pv-skaha.yaml
+kubectl apply -f storage-pv-skaha-cavern.yaml
+echo "Installing skaha"
+helm upgrade --install -n skaha-system --values skaha.yaml skaha science-platform/skaha
